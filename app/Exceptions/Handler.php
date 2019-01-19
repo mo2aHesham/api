05c078ec45fe439f6e0a58f375d9abae77acb545
momen <momen@momen.com>
2019-01-19 18:57:02 +0200

    handle exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,12 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +52,25 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($request->expectsJson()) {
+            if ($exception instanceof ModelNotFoundException) {
+             return response()->json([
+                "errors"=> 'Product Model Not Found'
+
+                ],Response::HTTP_NOT_FOUND);  
+        }
+        }
+         if ($request->expectsJson()) {
+            if ($exception instanceof NotFoundHttpException) {
+             return response()->json([
+                "errors"=> 'incorrect route'
+
+                ],Response::HTTP_NOT_FOUND);  
+        }
+        }
+
+        
+       // dd($exception);
         return parent::render($request, $exception);
     }
 }
--- app/Http/Requests/productRequest.php
+++ app/Http/Requests/productRequest.php
@@ -24,7 +24,7 @@ class productRequest extends FormRequest
     public function rules()
     {
         return [
-
+    
         'name'=>'required|max:255|unique:products',
         'description'=>'required',
         'price'=>'required|max:10',
